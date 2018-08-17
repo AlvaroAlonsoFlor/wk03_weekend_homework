22c90687e826eed84bcbf257b9cd6f9dad975a8d
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-08-17 20:37:39 +0100

    Add CRUD to screening

--- models/screening.rb
+++ models/screening.rb
@@ -1,63 +1,64 @@
-class Ticket
+class Screening
 
   attr_reader :id
-  attr_accessor :customer_id, :film_id
+  attr_accessor :film_id, :screening_time
 
   def initialize(info)
     @id = info['id'].to_i if info['id']
-    @customer_id = info['customer_id'].to_i
     @film_id = info['film_id'].to_i
+    @screening_time = info['screening_time'].to_i
+    @seats = info['seats'].to_i
   end
 
   def save
-    sql = "INSERT INTO tickets
-    (customer_id, film_id)
+    sql = "INSERT INTO screenings
+    (film_id, screening_time, seats)
     VALUES
-    ($1, $2)
+    ($1, $2, $3)
     RETURNING *"
-    values = [@customer_id, @film_id]
+    values = [@film_id, @screening_time, @seats]
     result = SqlRunner.run(sql, values)
     @id = result.first['id'].to_i
   end
 
   def update()
-    sql = "UPDATE tickets
-    SET customer_id = $1, film_id = $2
+    sql = "UPDATE screenings
+    SET film_id = $1, screening_time = $2, seats = $4
     WHERE id = $3 "
-    values = [@customer_id, @film_id, @id]
+    values = [@film_id, @screening_time, @id, @seats]
     SqlRunner.run(sql, values)
 
   end
 
   def delete()
-    sql = "DELETE FROM tickets
+    sql = "DELETE FROM screenings
     WHERE id = $1"
     values = [@id]
     SqlRunner.run(sql, values)
   end
 
-  def self.map_items(ticket_data)
-    ticket_data.map { |ticket| Ticket.new(ticket)  }
+  def self.map_items(screening_data)
+    screening_data.map { |screening| Screening.new(screening)  }
   end
 
   def self.all
-    sql = "SELECT * FROM tickets"
+    sql = "SELECT * FROM screenings"
     result = SqlRunner.run(sql)
     return self.map_items(result)
   end
 
   def self.delete_all
-    sql = "DELETE FROM tickets"
+    sql = "DELETE FROM screenings"
     SqlRunner.run(sql)
   end
 
   def self.find_by_id(id)
-    sql = "SELECT * FROM tickets
+    sql = "SELECT * FROM screenings
     WHERE id = $1"
     values = [id]
     result = SqlRunner.run(sql, values)
     if result.count > 0
-      ticket.map_items(result)
+      screening.map_items(result)
     else
       return nil
     end
