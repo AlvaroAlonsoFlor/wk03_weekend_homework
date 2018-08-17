a54cfe6903757edae2b19da210a3883965a9d3c6
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-08-17 11:47:02 +0100

    Add CRUD to film

--- console.rb
+++ console.rb
@@ -4,13 +4,21 @@ require_relative('models/film')
 require_relative('models/ticket')
 
 Customer.delete_all
+Film.delete_all
 
 customer1 = Customer.new({
   'name' => 'Alvaro',
   'funds' => 30
   })
 
+film1 = Film.new({
+  'title' => 'The Rock',
+  'price' => 5
+  })
+
+
 customer1.save
+film1.save
 
 binding.pry
 nil
---
+++ models/film.rb
@@ -0,0 +1,76 @@
+class Film
+
+  attr_reader :id
+  attr_accessor :title, :price
+
+  def initialize(info)
+    @id = info['id'].to_i if info['id']
+    @title = info['title']
+    @price = info['price'].to_i
+  end
+
+  def save
+    sql = "INSERT INTO films
+    (title, price)
+    VALUES
+    ($1, $2)
+    RETURNING *"
+    values = [@title, @price]
+    result = SqlRunner.run(sql, values)
+    @id = result.first['id'].to_i
+  end
+
+  def update()
+    sql = "UPDATE films
+    SET title = $1, genre = $2
+    WHERE id = $3 "
+    values = [@title, @price, @id]
+    SqlRunner.run(sql, values)
+
+  end
+
+  def delete()
+    sql = "DELETE FROM films
+    WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  # def customers()
+  #   sql = "SELECT films.* FROM films
+  #   INNER JOIN tickets
+  #   ON films.id = tickets.film_id
+  #   WHERE film_id = $1"
+  #   values = [@id]
+  #   result = SqlRunner.run(sql, values)
+  #   return Movie.map_items(result)
+  # end
+
+  def self.map_items(film_data)
+    film_data.map { |film| Film.new(film)  }
+  end
+
+  def self.all
+    sql = "SELECT * FROM films"
+    result = SqlRunner.run(sql)
+    return self.map_items(result)
+  end
+
+  def self.delete_all
+    sql = "DELETE FROM films"
+    SqlRunner.run(sql)
+  end
+
+  def self.find_by_id(id)
+    sql = "SELECT * FROM films
+    WHERE id = $1"
+    values = [id]
+    result = SqlRunner.run(sql, values)
+    if result.count > 0
+      film.map_items(result)
+    else
+      return nil
+    end
+  end
+
+end
